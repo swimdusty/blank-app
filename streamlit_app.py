56ba1e2a8318e0ef92ec4f39bcea56251fc604f7
Dustin Burgardt <78223996+swimdusty@users.noreply.github.com>
2024-10-28 23:57:13 -0500

    Update streamlit_app.py
    Added power supply calculations

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,12 +25,9 @@ st.sidebar.header("Adjust Parameters")
 TMS_width = st.sidebar.slider("TMS x-dimension (width) (meters)", min_value=5.0, max_value=7.0, step=1.0, value=7.0)
 PS_connector_length = st.sidebar.slider("Length of power supply interconnects (pos/neg)(meter)", min_value=1.0, max_value=15.0, step=1.0, value=4.0)
 PS_connector_efficiency = st.sidebar.slider("Interconnect resistivity compared to copper bars (%)", min_value=10.0, max_value=1000.0, step=10.0, value=100.0) / 100
+PS_number = st.sidebar.slider("Number of powersupplies (#)", min_value=1.0, max_value=6.0, step=1.0)
 corner_plate_resistance = st.sidebar.slider("Average resistance of corner plate (microohms)", min_value=10, max_value=1000, step=10, value=100) / 1000000
 corner_length = st.sidebar.slider("Length of longest corner interconnect (cm)", min_value=1.0, max_value=40.0, step=1.0, value=25.0) / 100
-#if power > 1000/power_supplies:
-#    power_supplies >= coil_breakdown
-#current_limit = st.sidebar.slider("Power Supply Current Limit (A)", min_value=10.0, max_value=2000.0, step=10.0, value=1000.0)
-#coil_breakdown = st.sidebar.slider("Number of coil sections", min_value=1.0, max_value=6.0, step=1.0, value=1.0)
 coil_temp = st.sidebar.slider("Expected temperature of coil", min_value=20.0, max_value=500.0, step=10.0, value=50.0)
 corner_resistance = corner_length * copper_resistivity * corner_interconnects / (copper_area * 0.35) #corner_length divided by 2 and 0.7 cross-sectional area
 corner = st.checkbox('Is corner connector a plate[Y] or stranded interconnect[N]', value=False)
@@ -44,15 +41,18 @@ coil_resistance = temp_dependent_resistivity * coil_length / copper_area
 PS_connector_resistance = temp_dependent_resistivity * PS_connector_length * PS_connector_efficiency / copper_area
 system_resistance = coil_resistance + PS_connector_resistance + corner_resistance
 
+#Power Supply Calculations
+system_resistancepps = system_resistance / PS_number
+
 # Button to recalculate
 if st.sidebar.button("Recalculate"):
     # Perform the recalculation
-    power, voltage = recalculate(system_resistance, coil_current)
+    power, voltage = recalculate(system_resistancepps, coil_current)
     
     # Display the results
-    st.write(f"**Calculated Power:** {power:.2f} kWatt")
-    st.write(f"**Calculated Voltage:** {voltage:.2f} V")
-    st.write(f"**System Resistance:** {system_resistance:.2f} Ohm")
+    st.write(f"**Calculated Power per PS:** {power:.2f} kWatt")
+    st.write(f"**Calculated Voltage per PS:** {voltage:.2f} V")
+    st.write(f"**Resistance per PS:** {system_resistancepps:.2f} Ohm")
     #st.line_chart(chart_data)
 
 # Instructions for the user
